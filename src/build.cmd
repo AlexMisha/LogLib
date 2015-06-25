5964af89f878fab44d1b4c206cb0d114a3208877
Misha <michaelalex600@gmail.com>
2015-06-25 15:46:45 +0300

    Remarks

--- src/build.cmd
+++ src/build.cmd
@@ -1,9 +1,12 @@
 @echo off
 
-ml /c /coff main\Log.asm || goto ErrLable
+ml /c /coff main\LogLib.asm
+lib /subsystem:windows /export:LogLib LogLib.obj
 
-goto End
-:ErrLable
-echo Error
-:End
+ml /c /coff test\TestLib.asm
+link /subsystem:windows TestLib.obj
+
+move TestLib.obj ignore
+move TestLib.exe ignore
+move LogLib.obj ignore
 pause
