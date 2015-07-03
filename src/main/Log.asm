9070672fd65d9f36424827398fd337f40c2cb5fa
Misha <michaelalex600@gmail.com>
2015-07-03 16:37:06 +0300

    Stack-call

--- src/main/Log.asm
+++ src/main/Log.asm
@@ -4,22 +4,28 @@ option casemap:none
 
 PUBLIC Log
 ;------------------------------------------------
-include \masm32\include\windows.inc
-include \masm32\include\user32.inc
+
 include \masm32\include\kernel32.inc
 include \masm32\macros\macros.asm
 includelib \masm32\lib\user32.lib
 includelib \masm32\lib\kernel32.lib
 
+EXTERN wsprintfA:NEAR
+
 .data
 
 .code
 
-Log proc near C FmtString:dword, args:vararg
-local buffer[256]:byte
+Log proc near C lpResultBuf:dword, FmtString:dword, args:vararg
 
-invoke wsprintfA, addr buffer, FmtString, args
-invoke OutputDebugString, addr buffer
+push dword ptr [ebp+20]
+push dword ptr [ebp+16]
+push FmtString
+push lpResultBuf
+call wsprintfA
+add esp, 16
+
+invoke OutputDebugString, [lpResultBuf]
 
 ret
 Log endp
--- src/test/TestLib.asm
+++ src/test/TestLib.asm
@@ -2,9 +2,8 @@
 .model flat, stdcall
 option casemap:none
 ;------------------------------------------------
-
-include \masm32\include\windows.inc
 include \masm32\include\user32.inc
+include \masm32\include\windows.inc
 include \masm32\include\kernel32.inc
 include \masm32\macros\macros.asm
 include c:\masm32\projects\LogLib\src\include\Log.inc
@@ -15,11 +14,13 @@ includelib \masm32\lib\kernel32.lib
 .data
 	Message db 'Hello', 0
 	Num db '442', 0
+	ResultBuf db 255
 	
 .code
 start:
-mov eax, 100h
-LOG_DEBUG "%s[%s]", offset Message, offset Num
+mov eax,1
+LOG_DEBUG addr ResultBuf, "%s[%08X]", addr Message, eax
+
 invoke ExitProcess, 0
 
 end start
