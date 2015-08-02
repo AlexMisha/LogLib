72ad687581ced8ad8feea78f50aa88c41bf69f3b
Misha <michaelalex600@gmail.com>
2015-08-02 15:35:48 +0300

    d

--- src/build.cmd
+++ src/build.cmd
@@ -1,8 +1,5 @@
 @echo off
 
-ml /c /coff main\Log.asm
-lib /subsystem:windows /export:Log Log.obj
-
 ml /c /coff /D LOG_LEVEL=4 test\TestLib.asm
 link /subsystem:windows /debug TestLib.obj
 
@@ -10,5 +7,4 @@ move TestLib.pdb ignore
 move TestLib.ilk ignore
 move TestLib.obj ignore
 move TestLib.exe ignore
-move Log.obj ignore
 pause
--- src/test/TestLib.asm
+++ src/test/TestLib.asm
@@ -2,24 +2,18 @@
 .model flat, stdcall
 option casemap:none
 ;------------------------------------------------
-include \masm32\include\user32.inc
-include \masm32\include\windows.inc
 include \masm32\include\kernel32.inc
 include \masm32\macros\macros.asm
 include c:\masm32\projects\LogLib\src\include\Log.inc
-includelib Log.lib
 includelib \masm32\lib\user32.lib
 includelib \masm32\lib\kernel32.lib
 
 .data
 	Message db 'Hello', 0
-	Num db '442', 0
-	ResultBuf db 255
 	
 .code
 start:
-mov eax,1
-LOG_DEBUG addr ResultBuf, "%s[%08X]", addr Message, eax
+LOG_DEBUG "%s[%08X]", addr Message, 1008h
 
 invoke ExitProcess, 0
 
